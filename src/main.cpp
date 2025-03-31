99759c359fba76d5f060b0ef7fb8396dbe3153fa
molimarco <mmolinari.internsip@aicenter.eu>
2025-03-31 10:13:03 +0200

    v1.1.2

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,4 @@
 #include <Arduino.h>
-#include <ESP32CAN.h>
-#include <CAN_config.h>
 #include <SPI.h>
 #include <SD.h>
 
@@ -17,7 +15,7 @@ const float DIVISOR_FACTOR = 5.057;
 unsigned long tiempoInicio = 0;
 bool calibrado = false;
 float VREF = 0.0;
-float sensibilidad = 0.0406;  // Fijado directamente
+float sensibilidad = 0.0406;
 
 const uint8_t N_MEDIANA = 15;
 float bufferMediana[N_MEDIANA] = {0.0};
@@ -37,20 +35,6 @@ const float alphaIIR = 0.2;
 
 float sumaVref = 0.0;
 unsigned long muestrasVref = 0;
-bool headerPrinted = false;
-
-CAN_device_t CAN_cfg;
-const int rx_queue_size = 10;
-
-uint8_t last_torque = 0;
-uint8_t last_duty = 0;
-uint8_t last_corriente = 0;
-float last_voltaje_CAN = 0.0;
-uint8_t last_switch = 0;
-uint8_t last_temp = 0;
-uint8_t last_angle_dir = 0;
-uint8_t last_mapa = 0;
-uint8_t last_error = 0;
 
 // ENCODER
 int encoderSteps = 100;
@@ -80,24 +64,6 @@ float medianaN(float* arr, uint8_t size) {
   return temp[size / 2];
 }
 
-void processCANMessages() {
-  CAN_frame_t rx_frame;
-  while (xQueueReceive(CAN_cfg.rx_queue, &rx_frame,  3 * portTICK_PERIOD_MS) == pdTRUE) {
-    if (rx_frame.MsgID == 0x290) {
-      last_torque = rx_frame.data.u8[0];
-      last_duty = rx_frame.data.u8[1];
-      last_corriente = rx_frame.data.u8[2];
-      last_voltaje_CAN = rx_frame.data.u8[3] * 0.1;
-      last_switch = rx_frame.data.u8[4];
-      last_temp = rx_frame.data.u8[5];
-    } else if (rx_frame.MsgID == 0x292) {
-      last_angle_dir = rx_frame.data.u8[0];
-      last_mapa = rx_frame.data.u8[3];
-      last_error = rx_frame.data.u8[4];
-    }
-  }
-}
-
 void setup() {
   Serial.begin(576000);
   analogReadResolution(12);
@@ -113,13 +79,6 @@ void setup() {
   Serial.println(" V/A");
   Serial.println("⚙️ Calibrando VREF durante 10 segundos, mantén el motor en idle...");
   tiempoInicio = millis();
-
-  CAN_cfg.speed = CAN_SPEED_250KBPS;
-  CAN_cfg.tx_pin_id = GPIO_NUM_27;
-  CAN_cfg.rx_pin_id = GPIO_NUM_26;
-  CAN_cfg.rx_queue = xQueueCreate(rx_queue_size, sizeof(CAN_frame_t));
-  ESP32Can.CANInit();
-  Serial.printf("✅ CAN SPEED: %d kbps\n", CAN_cfg.speed);
 }
 
 void loop() {
@@ -136,7 +95,7 @@ void loop() {
       Serial.print("✅ Calibración completada. VREF = ");
       Serial.print(VREF, 4);
       Serial.println(" V");
-      Serial.println("V_sensor(V),I_direct(A),V_mediana(V),I_filtrado1(A),I_filtrado2(A),I_IIR(A),V_real(V),Torque(bits),MotorDuty(%),I_CAN(A),V_CAN(V),SwitchPos,Temp(°C),DirAngle(bits),Mapa,Error,P(W),EncoderSteps,Pulsador");
+      Serial.println("V_sensor(V),I_direct(A),V_mediana(V),I_filtrado1(A),I_filtrado2(A),I_IIR(A),V_real(V),P(W),EncoderSteps,Pulsador");
       delay(1000);
     }
     return;
@@ -144,8 +103,6 @@ void loop() {
 
   if (ahora - t_anterior >= intervalo) {
     t_anterior = ahora;
-
-    processCANMessages();
 
     // === ENCODER ===
     int currentCLK = digitalRead(ENCODER_CLK);
@@ -200,20 +157,8 @@ void loop() {
     Serial.print(corrienteFiltrada2, 4);  Serial.print(",");
     Serial.print(corrienteIIR, 4);        Serial.print(",");
     Serial.print(voltajeReal, 3);         Serial.print(",");
-    Serial.print(last_torque);            Serial.print(",");
-    Serial.print(last_duty);              Serial.print(",");
-    Serial.print(last_corriente);         Serial.print(",");
-    Serial.print(last_voltaje_CAN, 1);    Serial.print(",");
-    Serial.print(last_switch);            Serial.print(",");
-    Serial.print(last_temp);              Serial.print(",");
-    Serial.print(last_angle_dir);         Serial.print(",");
-    Serial.print(last_mapa);              Serial.print(",");
-    Serial.print(last_error);             Serial.print(",");
     Serial.print(potencia, 4);            Serial.print(",");
     Serial.print(encoderSteps);           Serial.print(",");
     Serial.println(pulsadorValor);
-    
-    
-   
   }
 }
